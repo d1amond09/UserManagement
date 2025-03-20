fc4d0695a490e98d6b08b63519b8a8cd5d9e54b5
d1amond <max.berd.1900@gmail.com>
2025-03-20 11:21:07 +0300

    feat(core) add UserForUpdateDto

---
+++ UserManagement.Application/DTO/User/UserForUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagement.Application.DTO.User;
+
+public record UserForUpdateDto
+{
+	public Guid Id { get; init; }
+	public string Name { get; init; } = string.Empty;
+	public string Password { get; init; } = string.Empty;
+	public DateTime? LastLogin { get; init; }
+	public bool IsBlocked { get; init; }
+	public string? RefreshToken { get; init; }
+	public DateTime? RefreshTokenExpiryTime { get; init; }
+}
