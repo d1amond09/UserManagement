0a8666fbaedb82e71bfb62d43a3876d1baa5f2bf
d1amond <max.berd.1900@gmail.com>
2025-03-20 10:47:35 +0300

    feat(core): add mapping for Users

---
+++ UserManagement.Application/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UserManagement.Application.DTO.User;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Application;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<User, UserDto>();
+		CreateMap<UserForRegistrationDto, User>();
+	}
+}
+
