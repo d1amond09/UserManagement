15b37a8e60641f08acc0ee00c5dfb2fccf7dbf31
d1amond <max.berd.1900@gmail.com>
2025-03-20 10:42:20 +0300

    refactor(infrastructure): use UserConfiguration in AppDbContext

--- UserManagement.Infrastructure/Persistence/AppDbContext.cs
+++ UserManagement.Infrastructure/Persistence/AppDbContext.cs
@@ -1,53 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UserManagement.Domain.Entities;
+using UserManagement.Infrastructure.Persistence.Configurations;
 
 namespace UserManagement.Infrastructure.Persistence;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-	public virtual DbSet<User> Users { get; set; }
+	public DbSet<User> Users { get; set; }
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		modelBuilder.Entity<User>(entity =>
-		{
-			entity
-				.HasNoKey()
-				.ToTable("users");
-
-			entity.HasIndex(e => e.Id, "id").IsUnique();
-
-			entity.HasIndex(e => e.Email, "unique_email").IsUnique();
-
-			entity.Property(e => e.Email).HasColumnName("email");
-
-			entity.Property(e => e.Id)
-				.HasMaxLength(16)
-				.IsFixedLength()
-				.HasColumnName("id");
-
-			entity.Property(e => e.IsBlocked)
-				.HasDefaultValueSql("'0'")
-				.HasColumnName("is_blocked");
-
-			entity.Property(e => e.LastLogin)
-				.HasColumnType("datetime")
-				.HasColumnName("last_login");
-
-			entity.Property(e => e.Name)
-				.HasMaxLength(255)
-				.HasColumnName("name");
-
-			entity.Property(e => e.Password)
-				.HasMaxLength(255)
-				.HasColumnName("password");
-
-			entity.Property(e => e.RegistrationTime)
-				.HasDefaultValueSql("CURRENT_TIMESTAMP")
-				.HasColumnType("datetime")
-				.HasColumnName("registration_time");
-		});
-
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.ApplyConfiguration(new UserConfiguration());
 	}
 
 }
