84ee4f41b418a72031a2e2f4080c0f284a0817ea
d1amond <max.berd.1900@gmail.com>
2025-03-20 11:02:48 +0300

    feat(infrastructure): add RepositoryManager

---
+++ UserManagement.Domain/Contracts/Persistence/IRepositoryManager.cs
@@ -0,0 +1,8 @@
+﻿namespace UserManagement.Domain.Contracts.Persistence;
+
+public interface IRepositoryManager
+{
+	IUserRepository Users { get; }
+	Task SaveAsync();
+	void SaveChanges();
+}
---
+++ UserManagement.Infrastructure/Persistence/Repositories/RepositoryManager.cs
@@ -0,0 +1,21 @@
+﻿using UserManagement.Domain.Contracts.Persistence;
+
+namespace UserManagement.Infrastructure.Persistence.Repositories;
+
+public class RepositoryManager : IRepositoryManager
+{
+	private readonly AppDbContext _db;
+	private readonly Lazy<IUserRepository> _userRep;
+
+	public AppDbContext DbContext => _db;
+
+	public RepositoryManager(AppDbContext db)
+	{
+		_db = db;
+		_userRep = new Lazy<IUserRepository>(() => new UserRepository(_db));
+	}
+
+	public IUserRepository Users => _userRep.Value;
+	public async Task SaveAsync() => await _db.SaveChangesAsync();
+	public void SaveChanges() => _db.SaveChanges();
+}
