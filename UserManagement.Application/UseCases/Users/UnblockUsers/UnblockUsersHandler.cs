3c8476c1fe51f37cc586397eb70f605f5584096f
Maksim <max.berd.1900@gmail.com>
2025-03-21 10:57:46 +0300

    feat(core): add unblocking users

---
+++ UserManagement.Application/UseCases/Users/UnblockUsers/UnblockUsersHandler.cs
@@ -0,0 +1,27 @@
+﻿using UserManagement.Domain.Contracts.Persistence;
+using UserManagement.Application.Responses;
+using MediatR;
+
+namespace UserManagement.Application.UseCases.Users.UnblockUsers
+{
+	public class UnblockUsersHandler(IRepositoryManager rep) : IRequestHandler<UnblockUsersUseCase, ApiBaseResponse>
+	{ 
+		private readonly IRepositoryManager _rep = rep;
+
+		public async Task<ApiBaseResponse> Handle(UnblockUsersUseCase request, CancellationToken cancellationToken)
+		{
+			var users = await _rep.Users.GetByIdsAsync(request.UserIds, request.TrackChanges);
+			List<Guid> unblockedUserIds = [];
+
+			foreach (var user in users)
+			{
+				user.IsBlocked = false;
+				unblockedUserIds.Add(user.Id);
+			}
+
+			await _rep.SaveAsync();
+
+			return new ApiOkResponse<List<Guid>>(unblockedUserIds);
+		}
+	}
+}
---
+++ UserManagement.Application/UseCases/Users/UnblockUsers/UnblockUsersUseCase.cs
@@ -0,0 +1,7 @@
+﻿using UserManagement.Application.Responses;
+using MediatR;
+
+namespace UserManagement.Application.UseCases.Users.UnblockUsers;
+
+public sealed record UnblockUsersUseCase(IEnumerable<Guid> UserIds, bool TrackChanges) :
+	IRequest<ApiBaseResponse>;
